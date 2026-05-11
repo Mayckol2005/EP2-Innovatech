13184484b95ec701a36686ca210fa62c37ab2cc7
Mayckol2005 <may.mardones@duocuc.cl>
2026-05-10 23:38:08 -0400

    Preuba deploy

--- front_despacho/vite.config.js
+++ front_despacho/vite.config.js
@@ -7,10 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // Asegúrate de que esta sea la IP privada actual de tu EC2-Backend
         target: 'http://10.0.135.230:8081/', 
         changeOrigin: true,
-        // CORRECCIÓN: Se agregaron las barras / y la coma , correctamente
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
